a36f0b1b245b6f1e11c3e52c2fec7718a3e35745
Vincentcallaerts <57138252+Fattunatunatuna@users.noreply.github.com>
2021-02-25 15:04:05 +0100

    Oefening 6

---
+++ Oefening 6/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+
+namespace Oefening_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string antwoord;
+            int teller = 0;
+
+            Console.Write("Is er iemand thuis?  ");
+            antwoord = Console.ReadLine();
+            if (antwoord.Equals("ja") || antwoord.Equals("Ja"))
+            {
+                teller++;
+            }
+            Console.Write("Is er iemand thuis?  ");
+            antwoord = Console.ReadLine();
+            if (antwoord.Equals("ja") || antwoord.Equals("Ja"))
+            {
+                teller++;
+            }
+            Console.Write("Is er iemand thuis?  ");
+            antwoord = Console.ReadLine();
+            if (antwoord.Equals("ja") || antwoord.Equals("Ja"))
+            {
+                teller++;
+            }
+            Console.Write("Is er iemand thuis?  ");
+            antwoord = Console.ReadLine();
+            if (antwoord.Equals("ja") || antwoord.Equals("Ja"))
+            {
+                teller++;
+            }
+            Console.Write("Is er iemand thuis?  ");
+            antwoord = Console.ReadLine();
+            if (antwoord.Equals("ja") || antwoord.Equals("Ja"))
+            {
+                teller++;
+            }
+
+            if (teller >=3)
+            {
+                Console.WriteLine("Ah oke duidelijk er is iemand thuis");
+            }
+            else
+            {
+                Console.WriteLine("Oei toch niemand thuis dan");
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
