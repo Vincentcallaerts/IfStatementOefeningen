96f9cd55897388ff0a21e1d7b9a7eb0449ce529d
Vincentcallaerts <57138252+Fattunatunatuna@users.noreply.github.com>
2021-02-25 14:33:23 +0100

    Add project files.

---
+++ IfStatementOefeningen/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+namespace IfStatementOefeningen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int getal1, getal2;
+
+            Console.Write("Geef je eerste getal in: ");
+            getal1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Geef je twwede getal in: ");
+            getal2 = Convert.ToInt32(Console.ReadLine());
+
+            if (getal1 >= getal2)
+            {
+                Console.WriteLine($"Het grootste getal is: {getal1}");
+            }
+            else
+            {
+                Console.WriteLine($"Het grootste getal is: {getal2}");
+            }
+        }
+    }
+}
