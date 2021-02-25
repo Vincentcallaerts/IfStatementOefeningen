e34ec38658a5e4cf94fb43c548ed18758b284c29
Vincentcallaerts <57138252+Fattunatunatuna@users.noreply.github.com>
2021-02-25 14:36:55 +0100

    Oefening 2

---
+++ Oefening 2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oefening_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int getal1;
+
+            Console.Write("Hoeveel heb je gehaald: ");
+            getal1 = Convert.ToInt32(Console.ReadLine());
+
+            if (getal1 >= 5)
+            {
+                Console.WriteLine("Geslaagd!!");
+            }
+            else
+            {
+                Console.WriteLine("Gefaald!!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
