e9f29cf349e88087273fb5eabec73a156f0bff76
Vincentcallaerts <57138252+Fattunatunatuna@users.noreply.github.com>
2021-02-25 14:45:34 +0100

    oefening 3 en 4

---
+++ Oefening 3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oefening_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int getal1;
+
+            Console.Write("Geef een getal in: ");
+            getal1 = Convert.ToInt32(Console.ReadLine());
+
+            if (getal1 % 2 == 0)
+            {
+                Console.WriteLine("Dit is een even getal");
+            }
+            else
+            {
+                Console.WriteLine("Dit is een oneven getal");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Oefening 4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oefening_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double getal1;
+
+            Console.Write("Hoeveel kost de fiets: ");
+            getal1 = Convert.ToDouble(Console.ReadLine());
+
+            if (getal1 >= 400)
+            {
+                Console.WriteLine($"Prijs na korting: {getal1*0.8} ");
+            }
+            else
+            {
+                Console.WriteLine($"Korting pas vanaf 400 euro, fiets blijft {getal1} kosten");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
