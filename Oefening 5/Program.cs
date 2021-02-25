1109e9e2d6d863f180b9e408355e32378b19edf5
Vincentcallaerts <57138252+Fattunatunatuna@users.noreply.github.com>
2021-02-25 14:51:26 +0100

    Oefening 5

---
+++ Oefening 5/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oefening_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string antwoord;
+
+            Console.Write("Is er iemand thuis?  ");
+            antwoord = Console.ReadLine();
+
+
+            if (antwoord.Equals("ja") || antwoord.Equals("Ja"))
+            {
+
+                Console.Write("Is de situatie onder controle?  ");
+                antwoord = Console.ReadLine();
+
+                if (antwoord.Equals("ja") || antwoord.Equals("Ja"))
+                {
+                    Console.Write("Het alarm wordt uitgeschakeld");
+                }
+                else
+                {
+                    Console.Write("De brandweer wordt verwittigd");
+                }
+            }
+            else
+            {
+                Console.WriteLine("De brandweer wordt verwittigd");
+            }
+            Console.ReadLine();
+        }
+    }
+}
