a4321595c52a3d9d324e1b687c83bcf4a6c248e9
Vincentcallaerts <57138252+Fattunatunatuna@users.noreply.github.com>
2021-02-25 15:36:15 +0100

    Oefening 7

---
+++ Oefening 7/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oefening_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int getal1,getal2,getal3;
+            int temp;
+            string antwoord;
+
+            Console.Write("Eerste getal: ");
+            getal1 = Convert.ToInt32(Console.ReadLine());
+            
+            Console.Write("Tweede getal: ");
+            getal2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Derde getal: ");
+            getal3 = Convert.ToInt32(Console.ReadLine());
+
+            temp = Math.Min(getal1, Math.Min(getal2, getal3));
+            antwoord = $"{temp}";
+            temp = Math.Max(Math.Min(getal1, getal2), Math.Min(Math.Max(getal1, getal2), getal3));
+            antwoord = antwoord + $"-{temp}";
+            temp = Math.Max(getal1, Math.Max(getal2, getal3));
+            antwoord = antwoord + $"-{temp}";
+
+            Console.WriteLine(antwoord);
+            Console.ReadLine();
+
+        }
+    }
+}
